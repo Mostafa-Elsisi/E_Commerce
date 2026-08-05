25811b7003915862d869902875cafb63c067d583
Mostafa-Elsisi <elsisimostafa57@gmail.com>
2026-08-05 23:39:36 +0300

    Applay Session 03

--- E_Commerce.Domain/Contracts/IGenericRepository.cs
+++ E_Commerce.Domain/Contracts/IGenericRepository.cs
@@ -8,6 +8,8 @@ namespace E_Commerce.Domain.Contracts
         void Update(TEntity entity);
         void Remove(TEntity entity);
         Task<TEntity?> GetByIdAsync(TKey id,CancellationToken ct);
+        Task<TEntity?> GetByIdAsync(ISpecifications<TEntity, TKey> spec,CancellationToken ct);
         Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken ct);
+        Task<IReadOnlyList<TEntity>> GetAllAsync(ISpecifications<TEntity,TKey> spec,CancellationToken ct);
     }
 }
